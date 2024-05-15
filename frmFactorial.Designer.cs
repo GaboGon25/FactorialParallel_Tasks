d6e2cf1822511addc0d6a5890db65386763e3e2e
GaboGon25 <gabrieltablet969@gmail.com>
2024-05-14 22:04:26 -0600

    Agregar archivos de proyecto.

---
+++ frmFactorial.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace FactorialParallel_Tasks
+{
+    partial class frmFactorial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnCalcular = new Button();
+            txtNumero = new TextBox();
+            lblResultados = new Label();
+            SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(505, 79);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(276, 110);
+            btnCalcular.TabIndex = 0;
+            btnCalcular.Text = "CALCULAR";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(62, 115);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(404, 39);
+            txtNumero.TabIndex = 1;
+            // 
+            // lblResultados
+            // 
+            lblResultados.AutoSize = true;
+            lblResultados.Location = new Point(335, 336);
+            lblResultados.Name = "lblResultados";
+            lblResultados.Size = new Size(19, 32);
+            lblResultados.TabIndex = 2;
+            lblResultados.Text = ".";
+            // 
+            // frmFactorial
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(804, 589);
+            Controls.Add(lblResultados);
+            Controls.Add(txtNumero);
+            Controls.Add(btnCalcular);
+            Name = "frmFactorial";
+            Text = "frmFactorial";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnCalcular;
+        private TextBox txtNumero;
+        private Label lblResultados;
+    }
+}
---
+++ frmFactorial.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialParallel_Tasks
+{
+    public partial class frmFactorial : Form
+    {
+        public frmFactorial()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string input = txtNumero.Text;
+            var numeros = input.Split(',')
+                                .Select(numStr => int.Parse(numStr.Trim()));
+
+            // Usamos Task.WhenAll para calcular el factorial en paralelo
+            Task<long>[] tasks = numeros.Select(num => Task.Run(() => CalcularFactorial(num)))
+                                         .ToArray();
+
+            long[] resultados = await Task.WhenAll(tasks);
+
+            // Mostrar resultados
+            lblResultados.Text = "Resultados:\n";
+            for (int i = 0; i < numeros.Count(); i++)
+            {
+                lblResultados.Text += $"{numeros.ElementAt(i)}! = {resultados[i]}\n";
+            }
+        }
+
+        private long CalcularFactorial(int num)
+        {
+            long factorial = 1;
+            for (int i = 2; i <= num; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
+    }
+    
+}
